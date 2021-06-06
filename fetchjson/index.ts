602fec946c4872e0c65bc0139130140244496e60
colineckert <colineckert10@gmail.com>
2021-06-06 15:41:45 -0700

    add interface to file

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,7 +2,24 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+// interface that defines structure of an object
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 // async so we will receive a promise
 axios.get(url).then(res => {
-  console.log(res.data);
+  const todo = res.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+  `);
 });
