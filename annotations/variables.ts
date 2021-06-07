6938531ca05f00b30222223cde6b407f9cfcd8d4
colineckert <colineckert10@gmail.com>
2021-06-06 21:10:17 -0700

    add annotation for any type

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -30,3 +30,9 @@ let point: { x: number; y: number } = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{ "x": 10, "y": 20 }';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates); // { x: 10, y: 20 }
