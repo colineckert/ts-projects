35f6da2e6626e1c420119a59d914849a6fc054c1
colineckert <colineckert10@gmail.com>
2021-06-06 21:50:04 -0700

    function annotation with destructuring

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -1,3 +1,51 @@
 const add = (a: number, b: number): number => {
   return a + b;
 };
+
+// we can get inference on return values, but don't use it
+// we want it to error if we make mistake in return
+const subtract = (a: number, b: number) => {
+  a - b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+const throwError2 = (message: string): string => {
+  if (!message) {
+    throw new Error(message);
+  }
+
+  return message;
+};
+
+const todaysWeather = {
+  date: new Date(),
+  weather: 'sunny',
+};
+
+const logWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(todaysWeather);
