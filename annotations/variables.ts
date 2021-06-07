8f41cd1b6c8c531734c4ba2c4d9f4d062cb1f32d
colineckert <colineckert10@gmail.com>
2021-06-06 21:13:31 -0700

    add annotaion for later initialization

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -36,3 +36,14 @@ const logNumber: (i: number) => void = (i: number) => {
 const json = '{ "x": 10, "y": 20 }';
 const coordinates: { x: number; y: number } = JSON.parse(json);
 console.log(coordinates); // { x: 10, y: 20 }
+
+// 2) When we declare a variable on one line 
+// and initialize it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
