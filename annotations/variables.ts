77076d8bc84a92449d2f45b591cdfbae61b96ee8
colineckert <colineckert10@gmail.com>
2021-06-06 20:51:08 -0700

    add object annotations

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -8,3 +8,20 @@ let nothing: undefined = undefined;
 
 // built-in objects
 let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbesr: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car {
+
+}
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20
+};
