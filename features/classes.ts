eba15f96261fbfc5920f796762bd624f736bec75
colineckert <colineckert10@gmail.com>
2021-06-10 12:59:40 -0700

    create car class

--- features/classes.ts
+++ features/classes.ts
@@ -2,7 +2,20 @@ class Vehicle {
   drive(): void {
     console.log('chugga chugga');
   }
+
+  honk(): void {
+    console.log('beep');
+  }
 }
 
-const vehicle = new Vehicle();
-vehicle.drive();
+class Car extends Vehicle {
+
+}
+
+// const vehicle = new Vehicle();
+// vehicle.drive();
+// vehicle.honk();
+
+const car = new Car();
+car.drive();
+car.honk();
