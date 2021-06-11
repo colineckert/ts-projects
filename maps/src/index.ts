0e8e733928bd95d70343cca52bdf691418fb3bce
colineckert <colineckert10@gmail.com>
2021-06-11 12:17:17 -0700

    Create map object

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,8 +1,10 @@
-import { User } from './User';
-import { Company } from './Company';
+// import { User } from './User';
+// import { Company } from './Company';
 
-const user = new User();
-const companay = new Company();
-
-console.log(user);
-console.log(companay);
+const map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
