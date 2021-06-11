5b9d4acdeedc247e24bc2efdea34bd39eaea786c
colineckert <colineckert10@gmail.com>
2021-06-11 11:17:27 -0700

    learn modifiers in classes

--- features/classes.ts
+++ features/classes.ts
@@ -1,21 +1,32 @@
 class Vehicle {
-  drive(): void {
-    console.log('chugga chugga');
-  }
+  constructor(public color: string) {}
 
-  honk(): void {
+  protected honk(): void {
     console.log('beep');
   }
 }
 
-class Car extends Vehicle {
+const vehicle = new Vehicle('orange');
+console.log(vehicle.color);
 
+class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+
+  private drive(): void {
+    console.log('vroom');
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
 }
 
 // const vehicle = new Vehicle();
 // vehicle.drive();
 // vehicle.honk();
 
-const car = new Car();
-car.drive();
-car.honk();
+const car = new Car(4, 'red');
+car.startDrivingProcess();
