21b01e348ce42573b085710c835aed841f4373ca
colineckert <colineckert10@gmail.com>
2021-06-12 16:18:13 -0700

    Complete CharactersCollection class

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log(123);
+Object.defineProperty(exports, "__esModule", { value: true });
+var Sorter_1 = require("./Sorter");
+var CharactersCollection_1 = require("./CharactersCollection");
+// const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+var charactersCollection = new CharactersCollection_1.CharactersCollection('Xaayb');
+var sorter = new Sorter_1.Sorter(charactersCollection);
+sorter.sort();
+console.log(charactersCollection.data);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,2 +1,13 @@
-console.log(123);
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
+// const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('Xaayb');
+const sorter = new Sorter(charactersCollection);
+sorter.sort();
+console.log(charactersCollection.data);
