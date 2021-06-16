1e8b289f1304a7f2ec750b5640c57396e035ff8d
colineckert <colineckert10@gmail.com>
2021-06-16 16:29:53 -0700

    Update sort class

--- sort/src/Sorter.ts
+++ sort/src/Sorter.ts
@@ -5,15 +5,13 @@ interface Sortable {
 }
 
 export class Sorter {
-  constructor(public collection: Sortable) {}
-
   sort(): void {
-    const { length } = this.collection;
+    const { length } = this;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - 1 - i; j++) {
-        if (this.collection.compare(j, j + 1)) {
-          this.collection.swap(j, j + 1);
+        if (this.compare(j, j + 1)) {
+          this.swap(j, j + 1);
         }
       }
     }
