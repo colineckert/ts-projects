05f210dd94133b8421a928737d797dca71cb4901
colineckert <colineckert10@gmail.com>
2021-06-18 17:48:44 -0700

    finish sort project

--- sort/src/Sorter.ts
+++ sort/src/Sorter.ts
@@ -4,7 +4,11 @@ interface Sortable {
   swap(leftIndex: number, rightIndex: number): void;
 }
 
-export class Sorter {
+export abstract class Sorter {
+  abstract compare(leftIndex: number, rightIndex: number): boolean;
+  abstract swap(leftIndex: number, rightIndex: number): void;
+  abstract length: number;
+
   sort(): void {
     const { length } = this;
 
