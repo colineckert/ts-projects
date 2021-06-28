7af5fe0f1d7a76e34007d466b7f07f86632c9dfd
colineckert <colineckert10@gmail.com>
2021-06-28 12:56:50 -0700

    Write generic constraints

--- features/generics.ts
+++ features/generics.ts
@@ -45,3 +45,33 @@ function printAnything<T>(arr: T[]): void {
     console.log(arr[i]);
   }
 }
+
+printAnything<string>(['a', 'b', 'c']);
+
+
+// Generic Constraints
+
+class Car {
+  print() {
+    console.log('I am a car');
+  }
+}
+
+class House {
+  print() {
+    console.log('I am a house');
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]):void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+printHousesOrCars<House>([new House(), new House()]);
+printHousesOrCars<Car>([new Car(), new Car()]);
