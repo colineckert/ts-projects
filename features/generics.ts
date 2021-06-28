8c28d3e9d266a0516d4453b2ab73a53560bec4a3
colineckert <colineckert10@gmail.com>
2021-06-28 12:49:37 -0700

    Begin on generics

---
+++ features/generics.ts
@@ -0,0 +1,47 @@
+class ArrayOfNumbers {
+  constructor(public collection: number[]) {}
+
+  get(index: number): number {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfStrings {
+  constructor(public collection: string[]) {}
+
+  get (index: number): string {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfAnything<T> {
+  constructor(public collection: T[]) {}
+
+  get(index: number): T {
+    return this.collection[index];
+  }
+}
+
+// type inference
+const arr = new ArrayOfAnything(['a', 'b', 'c']);
+
+
+// Example of generics with functions
+
+function printStrings(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
