35ed2734e63b9ec67d0c20595714daedaddfbfdf
colineckert <colineckert10@gmail.com>
2021-06-29 07:56:22 -0700

    Right fetch method for user

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,15 +1,8 @@
-import { User } from "./models/User";
+import axios from 'axios';
 
-const user = new User({ name: 'myname', age: 20 });
+// axios.post('http://localhost:3000/users', {
+//   name: 'myname',
+//   age: 20
+// });
 
-user.on('change', () => {
-  console.log('Change #1');
-});
-user.on('change', () => {
-  console.log('Change #2');
-});
-user.on('save', () => {
-  console.log('Save was triggers');
-});
-
-user.trigger('save');
+axios.get('http://localhost:3000/users/1');
