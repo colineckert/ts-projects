8cf33f54bed9fe0a7b7bebe3b76ed6183561b374
colineckert <colineckert10@gmail.com>
2021-07-03 15:54:55 -0400

    initialize attrs in User

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,3 +1,4 @@
+import { Attributes } from './Attributes';
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
 
@@ -12,4 +13,5 @@ const rootUrl = 'http://localhost:3000/users';
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl)
+  public attributes: Attributes<UserProps> = new Attributes<UserProps>();
 }
