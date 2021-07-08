5a9c62355493c14f3a459c91b47e2fe32a2c603a
colineckert <colineckert10@gmail.com>
2021-07-07 20:08:06 -0700

    Setup save and fetch user functions

--- web/dist/src.f10117fe.js
+++ web/dist/src.f10117fe.js
@@ -142,6 +142,10 @@ function () {
     Object.assign(this.data, update);
   };
 
+  Attributes.prototype.getAll = function () {
+    return this.data;
+  };
+
   return Attributes;
 }();
 
@@ -2072,6 +2076,36 @@ function () {
     enumerable: false,
     configurable: true
   });
+
+  User.prototype.set = function (update) {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  };
+
+  User.prototype.fetch = function () {
+    var _this = this;
+
+    var id = this.attributes.get('id');
+
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without an id');
+    }
+
+    this.sync.fetch(id).then(function (response) {
+      _this.set(response.data);
+    });
+  };
+
+  User.prototype.save = function () {
+    var _this = this;
+
+    this.sync.save(this.attributes.getAll()).then(function (response) {
+      _this.trigger('save');
+    }).catch(function () {
+      _this.trigger('error');
+    });
+  };
+
   return User;
 }();
 
@@ -2086,14 +2120,15 @@ Object.defineProperty(exports, "__esModule", {
 var User_1 = require("./models/User");
 
 var user = new User_1.User({
-  name: 'new record',
+  id: 1,
+  name: 'newer name',
   age: 0
 });
 console.log(user.get('name'));
-user.on('change', function () {
-  console.log('User was changed');
+user.on('save', function () {
+  console.log(user);
 });
-user.trigger('change');
+user.save();
 },{"./models/User":"src/models/User.ts"}],"../../../../.nvm/versions/node/v14.16.1/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- web/src/index.ts
+++ web/src/index.ts
@@ -1,11 +1,11 @@
 import { User } from "./models/User";
 
-const user = new User({ name: 'new record', age: 0 });
+const user = new User({ id: 1, name: 'newer name', age: 0 });
 
 console.log(user.get('name'));
 
-user.on('change', () => {
-  console.log('User was changed');
+user.on('save', () => {
+  console.log(user);
 });
 
-user.trigger('change');
+user.save();
