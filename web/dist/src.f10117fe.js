5a0f2530dbc310b2e754c7ff03eb844bacbde2c5
colineckert <colineckert10@gmail.com>
2021-07-21 21:00:32 -0700

    bind event listeners in UserForm

--- web/dist/src.f10117fe.js
+++ web/dist/src.f10117fe.js
@@ -146,9 +146,28 @@ function () {
     return "\n      <div>\n        <h1>User Form</h1>\n        <input />\n        <button>Click Me</button>\n      </div>\n    ";
   };
 
+  UserForm.prototype.bindEvents = function (fragment) {
+    var eventsMap = this.eventsMap();
+
+    var _loop_1 = function _loop_1(eventKey) {
+      var _a = eventKey.split(':'),
+          eventName = _a[0],
+          selector = _a[1];
+
+      fragment.querySelectorAll(selector).forEach(function (element) {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    };
+
+    for (var eventKey in eventsMap) {
+      _loop_1(eventKey);
+    }
+  };
+
   UserForm.prototype.render = function () {
     var templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
+    this.bindEvents(templateElement.content);
     this.parent.append(templateElement.content);
   };
 
