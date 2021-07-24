d1fe69caefb56a3cf7a9a184907908e71588c20d
colineckert <colineckert10@gmail.com>
2021-07-24 14:18:32 -0700

    Create requireAuth and protected route

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,7 +1,17 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+  if (req.session?.loggedIn) {
+    next();
+    return;
+  }
+
+  res.status(403);
+  res.send('Not permitted');
 }
 
 const router = Router();
@@ -33,6 +43,33 @@ router.post('/login', (req: RequestWithBody, res: Response) => {
   } else {
     res.send('Invalid email or password');
   }
+});
+
+router.get('/', (req: Request, res: Response) => {
+  if (req.session?.loggedIn) {
+    res.send(`
+      <div>
+        <div>You are logged in</div>
+        <a href="/logout">Logout</a>
+      </div>
+    `);
+  } else {
+    res.send(`
+      <div>
+        <div>You are not logged in</div>
+        <a href="/login">Login</a>
+      </div>
+    `);
+  }
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+  req.session = undefined;
+  res.redirect('/');
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+  res.send('Welcome to protected route, logged in user!');
 });
 
 export { router };
