cc077030327ffab14af119c3ecdcf88a4b468d9e
colineckert <colineckert10@gmail.com>
2021-07-25 21:20:18 -0700

    Add logError

--- features/decorators.ts
+++ features/decorators.ts
@@ -5,13 +5,25 @@ class Boat {
     return `This boats color is ${this.color}`;
   }
 
-  @testDecorator
+  @logError('Something bad!')
   pilot(): void {
+    throw new Error();
     console.log('swish');
   }
 }
 
-function testDecorator(target: any, key: string): void {
-  console.log('Target:', target);
-  console.log('Key:', key);
+function logError(errorMessage: string) {
+  return function (target: any, key: string, desc: PropertyDescriptor): void {
+    const method = desc.value;
+
+    desc.value = function () {
+      try {
+        method();
+      } catch (e) {
+        console.log(errorMessage);
+      }
+    };
+  };
 }
+
+new Boat().pilot();
