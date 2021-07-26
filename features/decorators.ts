53b5bc6b7bdfeb1944f46a1fa04da23a7a8caec9
colineckert <colineckert10@gmail.com>
2021-07-25 21:30:11 -0700

    Write parameter and class decorators

--- features/decorators.ts
+++ features/decorators.ts
@@ -1,16 +1,32 @@
+@classDecorator
 class Boat {
   @testDecorator
   color: string = 'red';
 
+  @testDecorator
   get formattedColor(): string {
     return `This boats color is ${this.color}`;
   }
 
   @logError('Something bad!')
-  pilot(): void {
-    throw new Error();
-    console.log('swish');
+  pilot(
+    @parameterDecorator speed: string,
+    @parameterDecorator generateWake: boolean
+  ): void {
+    if (speed === 'fast') {
+      console.log('swish');
+    } else {
+      console.log('nothing');
+    }
   }
+}
+
+function classDecorator(constructor: typeof Boat) {
+  console.log(constructor);
+}
+
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log(key, index);
 }
 
 function testDecorator(target: any, key: string) {
