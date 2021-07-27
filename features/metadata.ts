2b207df66fadf09601019d552710837df44fe76c
colineckert <colineckert10@gmail.com>
2021-07-26 21:34:23 -0700

    Write first controller

--- features/metadata.ts
+++ features/metadata.ts
@@ -1,11 +1,50 @@
 import 'reflect-metadata';
 
-const plane = {
-  color: 'red',
-};
+// const plane = {
+//   color: 'red',
+// };
 
-Reflect.defineMetadata('note', 'hi there', plane);
+// Reflect.defineMetadata('note', 'hi there', plane);
+// Reflect.defineMetadata('height', 10, plane);
 
-const note = Reflect.getMetadata('note', plane);
+// const note = Reflect.getMetadata('note', plane);
+// const height = Reflect.getMetadata('height', plane);
 
-console.log(note);
+// console.log(note);
+// console.log(height);
+
+// Reflect.defineMetadata('note', 'hi there', plane, 'color');
+
+// const note = Reflect.getMetadata('note', plane, 'color');
+
+// console.log(note);
+
+// Class metadata
+@controller
+class Plane {
+  color: string = 'red';
+
+  @get('/login')
+  fly(): void {
+    console.log('vrrrrrrr');
+  }
+}
+
+function get(path: string) {
+  return function (target: Plane, key: string) {
+    Reflect.defineMetadata('path', path, target, key);
+  };
+}
+
+// const secret = Reflect.getMetadata('secret', Plane.prototype, 'fly');
+
+// console.log(secret);
+
+function controller(target: typeof Plane) {
+  for (let key in target.prototype) {
+    const path = Reflect.getMetadata('path', target.prototype, key);
+    const middleware = Reflect.getMetadata('middleware', target.prototype, key);
+
+    router.get(path, middleware, target.prototype[key]);
+  }
+}
