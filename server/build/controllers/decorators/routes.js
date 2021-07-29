e875cde3412c4694584e7a70d36e002aaab9d9a8
colineckert <colineckert10@gmail.com>
2021-07-28 21:09:40 -0700

    Create Methods enum

--- server/build/controllers/decorators/routes.js
+++ server/build/controllers/decorators/routes.js
@@ -1,10 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.get = void 0;
+exports.patch = exports.del = exports.post = exports.put = exports.get = void 0;
+var Methods_1 = require("./Methods");
 require("reflect-metadata");
-function get(path) {
-    return function (target, key, desc) {
-        Reflect.defineMetadata('path', path, target, key);
+function routerBinder(method) {
+    return function (path) {
+        return function (target, key, desc) {
+            Reflect.defineMetadata('path', path, target, key);
+            Reflect.defineMetadata('method', method, target, key);
+        };
     };
 }
-exports.get = get;
+exports.get = routerBinder(Methods_1.Methods.get);
+exports.put = routerBinder(Methods_1.Methods.put);
+exports.post = routerBinder(Methods_1.Methods.post);
+exports.del = routerBinder(Methods_1.Methods.del);
+exports.patch = routerBinder(Methods_1.Methods.patch);
--- server/src/controllers/decorators/routes.ts
+++ server/src/controllers/decorators/routes.ts
@@ -1,7 +1,17 @@
+import { Methods } from './Methods';
 import 'reflect-metadata';
 
-export function get(path: string) {
-  return function (target: any, key: string, desc: PropertyDescriptor) {
-    Reflect.defineMetadata('path', path, target, key);
+function routerBinder(method: string) {
+  return function (path: string) {
+    return function (target: any, key: string, desc: PropertyDescriptor) {
+      Reflect.defineMetadata('path', path, target, key);
+      Reflect.defineMetadata('method', method, target, key);
+    };
   };
 }
+
+export const get = routerBinder(Methods.get);
+export const put = routerBinder(Methods.put);
+export const post = routerBinder(Methods.post);
+export const del = routerBinder(Methods.del);
+export const patch = routerBinder(Methods.patch);
