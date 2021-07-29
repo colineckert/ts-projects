6770b1109a795c231ef5e39885895bfb7af8fe18
colineckert <colineckert10@gmail.com>
2021-07-28 21:12:38 -0700

    Create MetadataKeys enum

--- server/build/controllers/decorators/routes.js
+++ server/build/controllers/decorators/routes.js
@@ -1,13 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.patch = exports.del = exports.post = exports.put = exports.get = void 0;
+var MetadataKeys_1 = require("./MetadataKeys");
 var Methods_1 = require("./Methods");
 require("reflect-metadata");
 function routerBinder(method) {
     return function (path) {
         return function (target, key, desc) {
-            Reflect.defineMetadata('path', path, target, key);
-            Reflect.defineMetadata('method', method, target, key);
+            Reflect.defineMetadata(MetadataKeys_1.MetadataKeys.path, path, target, key);
+            Reflect.defineMetadata(MetadataKeys_1.MetadataKeys.method, method, target, key);
         };
     };
 }
--- server/src/controllers/decorators/routes.ts
+++ server/src/controllers/decorators/routes.ts
@@ -1,11 +1,12 @@
+import { MetadataKeys } from './MetadataKeys';
 import { Methods } from './Methods';
 import 'reflect-metadata';
 
 function routerBinder(method: string) {
   return function (path: string) {
     return function (target: any, key: string, desc: PropertyDescriptor) {
-      Reflect.defineMetadata('path', path, target, key);
-      Reflect.defineMetadata('method', method, target, key);
+      Reflect.defineMetadata(MetadataKeys.path, path, target, key);
+      Reflect.defineMetadata(MetadataKeys.method, method, target, key);
     };
   };
 }
