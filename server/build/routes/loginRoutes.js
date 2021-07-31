e77ac119ca6eff9d239aa1e7e9dfc9ce9ca92e98
colineckert <colineckert10@gmail.com>
2021-07-30 22:09:02 -0700

    Create RootController

--- server/build/routes/loginRoutes.js
+++ server/build/routes/loginRoutes.js
@@ -2,30 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 var express_1 = require("express");
-function requireAuth(req, res, next) {
-    var _a;
-    if ((_a = req.session) === null || _a === void 0 ? void 0 : _a.loggedIn) {
-        next();
-        return;
-    }
-    res.status(403);
-    res.send('Not permitted');
-}
 var router = express_1.Router();
 exports.router = router;
-router.get('/', function (req, res) {
-    var _a;
-    if ((_a = req.session) === null || _a === void 0 ? void 0 : _a.loggedIn) {
-        res.send("\n      <div>\n        <div>You are logged in</div>\n        <a href=\"/logout\">Logout</a>\n      </div>\n    ");
-    }
-    else {
-        res.send("\n      <div>\n        <div>You are not logged in</div>\n        <a href=\"/login\">Login</a>\n      </div>\n    ");
-    }
-});
-router.get('/logout', function (req, res) {
-    req.session = undefined;
-    res.redirect('/');
-});
-router.get('/protected', requireAuth, function (req, res) {
-    res.send('Welcome to protected route, logged in user!');
-});
