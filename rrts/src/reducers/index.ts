87b503b40302ab712d3a20bfebe68e6cc3161d99
colineckert <colineckert10@gmail.com>
2021-08-01 16:44:26 -0700

    Connect _App with reducers

--- rrts/src/reducers/index.ts
+++ rrts/src/reducers/index.ts
@@ -1,6 +1,11 @@
 import { combineReducers } from 'redux';
 import { todosReducer } from './todos';
+import { Todo } from '../actions';
 
-export const reducers = combineReducers({
+export interface StoreState {
+  todos: Todo[];
+}
+
+export const reducers = combineReducers<StoreState>({
   todos: todosReducer,
 });
