4859bb7b34bb4a68846a533fd1a46026a09d710b
colineckert <colineckert10@gmail.com>
2021-07-31 22:20:41 -0700

    Add state to component

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -6,8 +6,24 @@ interface AppProps {
 }
 
 class App extends React.Component<AppProps> {
+  state = { counter: 0 };
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
   render() {
-    return <div>{this.props.color}</div>;
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
   }
 }
 
