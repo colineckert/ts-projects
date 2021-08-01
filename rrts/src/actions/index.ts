8057c276f0c8f7cea3631284795186cda0a5edf9
colineckert <colineckert10@gmail.com>
2021-08-01 10:58:08 -0700

    Create FetchTodosAction interface to use in reducers

--- rrts/src/actions/index.ts
+++ rrts/src/actions/index.ts
@@ -1,10 +1,16 @@
 import axios from 'axios';
 import { Dispatch } from 'redux';
+import { ActionTypes } from './types';
 
-interface Todo {
+export interface Todo {
   id: number;
   title: string;
   completed: boolean;
+}
+
+export interface FetchTodosAction {
+  type: ActionTypes.fetchTodos;
+  payload: Todo[];
 }
 
 const url = 'https://jsonplaceholder.typicode.com/todos';
@@ -13,8 +19,8 @@ export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
     const response = await axios.get<Todo[]>(url);
 
-    dispatch({
-      type: 'FETCH_TODOS',
+    dispatch<FetchTodosAction>({
+      type: ActionTypes.fetchTodos,
       payload: response.data,
     });
   };
