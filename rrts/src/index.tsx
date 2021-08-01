cb609058ec9ff75b8e3adf1b008b07f739b0ec6d
colineckert <colineckert10@gmail.com>
2021-07-31 22:16:54 -0700

    init project

---
+++ rrts/src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+interface AppProps {
+  color?: string;
+}
+
+class App extends React.Component<AppProps> {
+  render() {
+    return <div>{this.props.color}</div>;
+  }
+}
+
+ReactDOM.render(<App color="red" />, document.querySelector('#root'));
